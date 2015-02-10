7bcfb75d099d17087c910efce1ec8584d80a02c5
Grant Gunyon <gunyongj10@uww.edu>
2015-02-10 16:22:58 -0600

    My version 1.0

--- main.cpp
+++ main.cpp
@@ -1,9 +1,25 @@
 #include <iostream>
+#include <cstdlib>
+#include <iomanip>
+#include <stdio.h>
+#include <stdlib.h>
 
 using namespace std;
 
 int main()
 {
-    cout << "Hello world!" << endl;
-    return 0;
+        int number = 0;
+        cout << "Guess a number between 1 and 100: ";
+        cin >> number;
+        srand((unsigned)time(0));
+
+        int randomnumber = rand()%100+1;
+
+        if (number == randomnumber)
+                cout << "You guessed right, good job" << endl;
+        else if (number != randomnumber)
+                cout << "You guessed the wrong number" << endl;
+
+
+return 0;
 }
